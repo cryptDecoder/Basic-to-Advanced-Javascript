ee96a829f5ca014491e394b869bbacf27a6927ed
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-26 22:47:33 +0530

    Array

---
+++ Intermediate Level/array.js
@@ -0,0 +1,23 @@
+/**
+ * Array:-> 
+ *  Array is a just collection of variable or values.
+ */
+
+ //array declaration
+ var countries = ['India','USA','Brazil','Japan','Russia']
+
+ // array declaration
+ var games = new Array(
+     'Cricket',
+     'Football',
+     'Basket Ball'
+ )
+
+ // display the array elements
+ console.log(countries);
+ console.log(games);
+
+ // accessing specific value from array
+console.log(countries[2]);
+// count the elements
+console.log(games.length);
