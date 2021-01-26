f2d021024bc0efd9a7f038f240eb2674bcd0c4f1
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-26 18:58:59 +0530

    Conditional Roles done

---
+++ Basics/ConditionalRoles.js
@@ -0,0 +1,29 @@
+/**
+ *  CONDITIONAL ROLES
+ *      create an application  with following roles.
+ *      1. admin -> get full access
+ *      2. subadmin -> get access to create and delete the course.
+ *      3. testprep -> get access to create/delete tests
+ *      4. user -> get access the consume content
+ */
+
+
+ var user = 'admin'; // change the user value to check user role and permissions
+
+ switch (user) {
+    case "admin":
+        console.log("you get the full access.");         
+        break;
+    case "subadmin":
+        console.log("you get the access create and delete the courses.");
+        break;
+    case "testprep":
+        console.log("You get access to create and delete test");
+        break;
+    case "user":
+        console.log("You can consume the content");
+        break;
+     default:
+         console.log("Not matching user");
+         break;
+ }
