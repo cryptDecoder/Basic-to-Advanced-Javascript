73d2ed391aba8dd46a1c0ae60f2fe1fbf54a32a0
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-26 17:57:22 +0530

    Discount calculation of price done

---
+++ Basics/Discount.js
@@ -0,0 +1,16 @@
+/**
+ *  In this section we are going to learn the discount of price.
+ */
+
+ console.log("# ----- DISCOUNT CALCULATION ON PRICE ----- #");
+
+
+ var sellingPrice = 200;
+ var listingPrice = 799;
+ 
+ var discountPercent = ((listingPrice - sellingPrice) / listingPrice) * 100;
+
+ console.log("Discount price is :"+discountPercent);
+
+ absDiscount = Math.round(discountPercent)
+ console.log("Round discount : "+absDiscount);
