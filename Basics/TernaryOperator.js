ac835dd3a7a4f63fa954e4ec5481bfc691619a16
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-26 18:43:48 +0530

    Ternary operator done

---
+++ Basics/TernaryOperator.js
@@ -0,0 +1,14 @@
+/**
+ *  In this section we are going to learn the Ternary operator
+ *  In given example we are going to show is user is authenticated show user sign out button otherwise show login options.
+ */
+
+ var authenticated =  false; // change with true for understanding the conditions.
+
+ if (authenticated) {
+     console.log("Show sign out button.");
+ } else {
+     console.log("Show login options.");
+ }
+
+ authenticated ? console.log("Sign out Button") : console.log("Login button");
