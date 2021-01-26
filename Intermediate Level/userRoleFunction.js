dc9d1ffa75c1294a9846962e682965d7d9bba2c2
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-26 20:52:00 +0530

    userRole function

---
+++ Intermediate Level/userRoleFunction.js
@@ -0,0 +1,11 @@
+/**
+ *  Define a function that can answer the role of a user
+ * A user can be on one following roles:
+ * admin
+ * subadmin
+ * testprep
+ * user
+ * other
+ * 
+ * Input: getUserRole(name, role)
+ */
