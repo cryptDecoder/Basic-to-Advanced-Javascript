0e631f805a5b606b6a39a742e934c497fbb42d6a
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-26 17:50:09 +0530

    Simple operators are done

--- Basics/Operators.js
+++ Basics/Operators.js
@@ -1,3 +1,17 @@
 // Operators in javascript
 
+/**
+ *  in this section we are going to understand the operators in javascript.
+ */
+
 console.log('# ----- OPERATORS IN JAVASCRIPT ----#');
+
+var num1 = 10;
+var num2 = 20;
+
+console.log(`
+Multiplication of numbers: ${num1 * num2}
+`);
+
+var value = num1 < num2;
+console.log(value);
