fe3257a733c131f57fdd6f094a4928d277e69232
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-26 19:26:43 +0530

    done

--- Basics/TruthyAndFalsy.js
+++ Basics/TruthyAndFalsy.js
@@ -5,3 +5,20 @@
  *  0
  *  NaN
  */
+
+
+ var user = null; // change to check results.
+
+ if (user) {
+    console.log("Condition is true");
+ }
+
+
+ console.log(2 + "2"); // truthy values
+
+ var value = '2' //checking the coercion  replace == to == for proper output
+
+ if (2 === value) {
+     console.log("Code running");
+ }
+
