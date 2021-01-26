e55c9a3842158ba21c930b6025d3dbaa4597812b
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-26 19:38:06 +0530

    Function part is done

--- Intermediate Level/functions.js
+++ Intermediate Level/functions.js
@@ -4,6 +4,30 @@
  *  
  */
 
+ // declaration of function
 function sayHello(){
-    console.log();
+    console.log("Hello, This is a first javascript function.");
 }
+
+// calling the function
+sayHello()
+
+// function with parameter
+
+function sayHelloName(name){
+    console.log(`Hello, ${name}. how are you?`)
+}
+
+//calling the function
+sayHelloName("Pruthviraj")
+
+
+// function with return
+
+function namstey(){
+    return "namstey, how are you all?"
+}
+
+// calling function and store result into variables
+var greet = namstey();
+console.log(greet);
