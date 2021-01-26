38ce60ec37a9ff0b0eae26926b094fcec4eab569
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-27 01:14:06 +0530

    Objects

--- Intermediate Level/objects.js
+++ Intermediate Level/objects.js
@@ -1,5 +1,8 @@
 /**
  * Objects in javascript -> 
+ * 
+ * declare objects in javascript 
+ * var objectName = {};
  */
 
 // declare the object
@@ -23,3 +26,34 @@ console.log(user.loginCount);
 
 console.log(user);
 console.table(user)
+
+
+/***
+ * objects part 2
+ */
+
+// declare the object
+var user2 = {
+    firsName: 'John',
+    lastName: 'Stark',
+    email: 'John@gmail.com',
+    role: 'user',
+    loginCount: 27,
+    facebook: true,
+    course: [],
+    buyCourse: function (courseName) {
+        this.course.push(courseName)
+    },
+    getCourseCount: function () {
+        return `${this.firsName} is enrolled in total of ${this.course.length}`
+    },
+
+};
+
+console.log(user2.getCourseCount());
+user2.buyCourse("Javascript")
+user2.buyCourse("Python")
+user2.buyCourse("MongoDB")
+console.log(user2.getCourseCount());
+
+console.table(user2)
