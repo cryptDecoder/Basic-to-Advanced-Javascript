2c2a6a3e2d1ae378c068d3bb1da506f61ebb3234
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-27 01:02:21 +0530

    objects

--- Intermediate Level/objects.js
+++ Intermediate Level/objects.js
@@ -1,3 +1,25 @@
 /**
- * Objects in javascript
+ * Objects in javascript -> 
  */
+
+// declare the object
+var user = {
+    firsName: 'Prithvi',
+    lastName: 'Sonawane',
+    email: 'pruthvirajs2007@gmail.com',
+    role: 'admin',
+    loginCount: 23,
+    facebook: true,
+};
+
+// access object
+
+console.log(user.firsName);
+console.log(user["email"]); // always wrap with double quote 
+
+console.log(user.loginCount);
+user.loginCount = 44;
+console.log(user.loginCount);
+
+console.log(user);
+console.table(user)
