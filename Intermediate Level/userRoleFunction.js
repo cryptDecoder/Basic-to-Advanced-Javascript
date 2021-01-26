9a7ff03020c1f77fa5d30cfa675fc6e1b5333206
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-26 21:10:53 +0530

    Userolefunction

--- Intermediate Level/userRoleFunction.js
+++ Intermediate Level/userRoleFunction.js
@@ -9,3 +9,40 @@
  * 
  * Input: getUserRole(name, role)
  */
+
+ // declare the function here
+ function getUserRole(name,role){
+     switch (role) {
+        case "admin":
+             return `${name} is admin with all access.`
+             break;
+        case "subadmin":
+            return `${name} is subadmin with access to create and delete course`
+            break;
+        case "testprep":
+            return `${name} is testprep with access to create and delete test`
+            break;
+        case "user":
+            return `${name} is a user with access to consume the content `
+         default:
+             return `${user} is a trial user`
+             break;
+     }
+ }
+
+// calling the function
+
+var getRole = getUserRole('John',"admin")
+console.log(getRole);
+
+// declare function as a variable 
+
+var getRoleValue = function (name,role){
+    if (role == "admin") {
+        return `${name} has admin access.`
+    } else {
+        return `${name} it may be another type of user`
+    }
+}
+
+console.log(getRoleValue("mac","admin"));
