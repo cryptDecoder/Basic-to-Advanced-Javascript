2a0d184bdf10ba895116233d5d5b3e31520d46b6
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-26 18:24:13 +0530

    Conditional Login

---
+++ Basics/ConditionalLogin.js
@@ -0,0 +1,19 @@
+
+ /**
+  *  What is conditional in javascript
+  *     check the given values and take the action as per the request.
+  *     for ex. if logged with google account send the email using gmail else send with outlook.
+  * 
+  *     for give following example
+  *         if one of the login is false the login will failed otherwise login is success.
+  */
+
+  var email = false;
+  var facebook = true;
+  var google = false;
+
+  if(email || facebook || google){
+      console.log("Login success");
+  }else{
+      console.log("Login failed");
+  }
