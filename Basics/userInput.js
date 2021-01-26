d793cca65d6b77520227c3139a61ff3fa83746b2
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-26 17:40:46 +0530

    User values show

---
+++ Basics/userInput.js
@@ -0,0 +1,23 @@
+// USER INPUT
+
+console.log("# ----- USER INPUT ----- #");
+
+const uid = "abc123";
+var fullName = "Pruthviraj Sonawane";
+var email = "pruthvirajs2007@gmail.com";
+var password = "12345";
+var confirmPassword = "12345";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+// display the information
+
+console.log(fullName);
+console.log(email);
+
+console.log(`
+    with unique id:${uid}
+    user is:${fullName}
+    email is:${email}
+    password is:${password}
+`);
