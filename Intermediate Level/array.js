323687b93489a1da4bbef04c3f0f0e16bc9e51f7
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-26 23:09:09 +0530

    Array part 2

--- Intermediate Level/array.js
+++ Intermediate Level/array.js
@@ -35,3 +35,39 @@ console.log(games.shift());
 // indexof 
 
 console.log(countries.indexOf("India"));
+
+/**
+ *  Array part 2
+ */
+
+function isEven(element) {
+    if (element % 2 === 0) {
+        return true;
+    }
+    return false;
+}
+
+console.log(isEven(2));
+
+// Single line function
+
+function isOdd(ele) {
+    return ele % 2 === 1;
+}
+console.log(isOdd(3));
+
+// callback and arrow function
+
+var isEvenNumber = (number) => {
+    return number % 2 === 0;
+}
+
+console.log(isEvenNumber(2));
+
+var result = [2, 3, 4, 5, 6, 7, 8, 9].every(isEven)
+console.log(result);
+
+var output = [2, 4, 6].every((e) => {
+    return e % 2 === 0;
+});
+console.log(output);
