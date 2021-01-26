03f1e50f35e82b8168acb6271fe7114cb1d72427
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-26 21:45:32 +0530

    Javascript Context

--- Intermediate Level/context.js
+++ Intermediate Level/context.js
@@ -1,4 +1,14 @@
 /**
  * Context in javascript
- *
+ * 
  */
+
+ if (2 === '2') {
+    console.log("This is true");     
+ }
+
+ var value = "perry";
+ 
+ if (value === window.value) {
+    console.log("This is true again");
+ }
