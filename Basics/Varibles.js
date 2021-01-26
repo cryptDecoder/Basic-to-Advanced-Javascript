c4501ece5f801c6d318c3d5c821ef591c8cbdf26
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-26 17:23:23 +0530

    variables

---
+++ Basics/Varibles.js
@@ -0,0 +1,17 @@
+// # VARIABLE AND DATA TYPES
+console.log("# ----- VARIABLE AND DATA TYPES ----- #");
+/*
+    Variable -> 
+    var, let and const are used to declare variable
+    var -> declare general purpose variable.
+    
+*/
+var fullName = "Pruthviraj Sonawane";
+//How to name Variables
+/*
+    fullName
+    full-name
+    full_name
+    never use upper case for start variable name.
+*/
+console.log("Your full name is  : ",fullName);
