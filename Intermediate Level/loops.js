da0f98d2c93e9b7ee72c0a244793fecc13ab4c2f
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-27 09:54:47 +0530

    for loop

--- Intermediate Level/loops.js
+++ Intermediate Level/loops.js
@@ -2,3 +2,14 @@
  *  Loops in javascript
  *  for loop
  */
+
+// basic for loop
+
+for (let i = 0; i < 10; i++) {
+    console.log("Basic for loop");
+}
+
+const names = ['Ted', 'Tom', 'Hardy', 'John'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
