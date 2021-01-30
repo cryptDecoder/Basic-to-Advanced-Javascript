0cebf69bb3f8f9ef0e8dc594ed347402225fd077
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-30 11:01:54 +0530

    scope

---
+++ AdvancedJs/scope.js
@@ -0,0 +1,13 @@
+/**
+ * Scope in javascript:
+ *  Scope is a big issue in javascript
+ */
+
+console.log(name);
+var name = "prithvi"
+
+if (true) {
+    let lastName = "Sonawane"
+    console.log(lastName);
+}
+console.log(lastName);
