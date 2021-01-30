c34da8742fc65cbb5ff86ef82fe037b634bfd0b2
cryptDecoder <pruthvirajs2007@gmail.com>
2021-01-30 18:37:37 +0530

    API test

---
+++ AdvancedJs/classObject.js
@@ -0,0 +1,10 @@
+//import User from './classes'
+
+const User = require('./classes.js')
+
+const prithvi = new User("Prithvi", "pruthvirajs2007@gmail.com")
+
+
+prithvi.enrollCourse("Javascript")
+
+console.log(prithvi);
--- AdvancedJs/spread.js
+++ AdvancedJs/spread.js
@@ -4,3 +4,16 @@
 
 var maxValue = Math.max(2, 3, 5, 6, 7, 77)
 console.log(maxValue);
+
+
+function sumValues(...args) {
+    console.log(args);
+    let sum = 0;
+    for (let arg of args) {
+        sum = sum + arg
+    }
+    return sum
+
+}
+
+console.log(sumValues(22, 33, 33));
